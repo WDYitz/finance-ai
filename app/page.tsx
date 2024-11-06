c2b223c6242fddf5f379abcfef043307d6c0535a
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-11-06 13:21:16 -0300

    chore: add tailwindcss prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-
 const Home = () => {
   return (
-    <main className="flex flex-col gap-8 items-center sm:items-start">
+    <main className="flex flex-col items-center gap-8 sm:items-start">
       <h2>Setup</h2>
     </main>
   );
-}
+};
 
 export default Home;
