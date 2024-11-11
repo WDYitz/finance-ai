888538c02264a8c4e1a9fa62faee1b46a58a02be
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-11-11 13:14:13 -0300

    Refactor: Add Percentage Item component for displaying percentage items

---
+++ app/(home)/_components/expenses-per-category.tsx
@@ -0,0 +1,34 @@
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { TRANSACTION_CATEGORY_LABELS } from "@/constants/transactions";
+import type { ITotalExpensePerCategory } from "@/services/interfaces/total-expense-per-category";
+
+interface ExpensesPerCategoryProps {
+  expensesPerCategory: ITotalExpensePerCategory[];
+}
+
+const ExpensesPerCategory = ({ expensesPerCategory }: ExpensesPerCategoryProps) => {
+  return (
+    <ScrollArea className="col-span-2 rounded-md border pb-6 h-full">
+      <CardHeader>
+        <CardTitle className="font-bold">Gastos per Categoria</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-6">
+        {
+          expensesPerCategory.map(category => (
+            <div key={category.category} className="space-y-2">
+              <div className="flex justify-between w-full">
+                <p className="font-bold text-sm">{TRANSACTION_CATEGORY_LABELS[category.category as keyof typeof TRANSACTION_CATEGORY_LABELS]}</p>
+                <p className="text-sm font-bold">{category.percentageOfTotal}%</p>
+              </div>
+              <Progress value={category.total} />
+            </div>
+          ))
+        }
+      </CardContent>
+    </ScrollArea>
+  );
+}
+
+export default ExpensesPerCategory;
---
+++ app/(home)/_components/transactions-pie-chart.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import { chartConfig } from "@/constants/transactions"
+import type { TypesPercentageType } from "@/services/interfaces/transaction-percentage-type"
+import { TransactionType } from "@prisma/client"
+import { PiggyBankIcon, TrendingDownIcon, TrendingUpIcon } from "lucide-react"
+import PercentageItem from "./percentage-item"
+
+interface TransactionPieChartProps {
+  depositTotal: number
+  expensesTotal: number
+  investmentTotal: number
+  typesPercentage: TypesPercentageType
+}
+
+const TransactionPieChart = ({ depositTotal, expensesTotal, investmentTotal, typesPercentage }: TransactionPieChartProps) => {
+  const chartData = [
+    { type: TransactionType.INVESTMENT, amount: investmentTotal, fill: "#FFFFFF" },
+    { type: TransactionType.DEPOSIT, amount: depositTotal, fill: "#55B02E" },
+    { type: TransactionType.EXPENSE, amount: expensesTotal, fill: "#E93030" },
+  ]
+
+  return (
+    <Card className="flex flex-col p-12">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="amount"
+              nameKey="type"
+              innerRadius={60}
+            />
+          </PieChart>
+        </ChartContainer>
+        <div className="space-y-2">
+          <PercentageItem icon={<TrendingUpIcon size={16} className="text-primary" />} title="Receita" percentage={typesPercentage[TransactionType.DEPOSIT]} />
+          <PercentageItem icon={<TrendingDownIcon size={16} className="text-danger" />} title="Despesa" percentage={typesPercentage[TransactionType.EXPENSE]} />
+          <PercentageItem icon={<PiggyBankIcon size={16} className="text-muted-foreground" />} title="Investimento" percentage={typesPercentage[TransactionType.INVESTMENT]} />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default TransactionPieChart;
